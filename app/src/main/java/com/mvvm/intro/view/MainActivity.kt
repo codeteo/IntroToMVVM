ce32d3038bca10ae50c7c90d3040f3a8613ff18d
codeteo <ramones.teo@gmail.com>
2018-06-12 00:30:34 +0300

    Binds the view to viewModel actions

--- app/src/main/java/com/mvvm/intro/view/MainActivity.kt
+++ app/src/main/java/com/mvvm/intro/view/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.mvvm.intro.view
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.mvvm.intro.R
+import com.mvvm.intro.databinding.ActivityMainBinding
+import com.mvvm.intro.viewmodel.CalculatorViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.vm = CalculatorViewModel()
     }
 }
